35478b49aa21811590dcf4ebfb05c6fc8cb135c6
unknown <shukumar-542>
2022-03-07 22:17:30 +0600

    use reducer hook

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import ReduceCount from './components/ReducerCount/ReduceCount';
+import { useReducer } from 'react';
+
+const initialState = {count :0}
+const reducer = (state, action) =>{
+
+}
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ReduceCount/>
     </div>
   );
 }
---
+++ src/components/ReducerCount/ReduceCount.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ReduceCount = () => {
+      return (
+            <div>
+                  <h1>This is reducer count</h1>
+            </div>
+      );
+};
+
+export default ReduceCount;
